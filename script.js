b22ddf9be69fc5b51b6b55f67d46e7b1f4d897f4
crab79 <hanyaoyao0709@gmail.com>
2023-06-23 17:41:01 +0800

    Finished all assignment requests

--- script.js
+++ script.js
@@ -1,162 +1,172 @@
-var numberButtons = document.getElementsByClassName("number");
-var operatorButtons = document.getElementsByClassName("operator");
-var equalsButton = document.getElementById("button-equals");
-var clearButton = document.getElementById("button-clear");
-var backspaceButton = document.getElementById("button-backspace");
-var screen = document.getElementById("screen");
+const numberButtons = Array.from(document.getElementsByClassName("number"));
+const operatorButtons = Array.from(document.getElementsByClassName("operator"));
+const equalsButton = document.getElementById("button-equals");
+const clearButton = document.getElementById("button-clear");
+const backspaceButton = document.getElementById("button-backspace");
+const screen = document.getElementById("screen");
+const dot = document.getElementById("button-point");
 
-var firstNums = 0;
-var secondNums = 0;
-var currentNums = [];
-var operator = "";
-var decimalPressed = false;
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let hasDecimal = false; // Flag to track if decimal point has been used
 
-function convertToNumber() {
-  var numStr = currentNums.join("");
-  return decimalPressed ? parseFloat(numStr) : parseInt(numStr);
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function calculateResult() {
-  switch (operator) {
-    case "+":
-      return firstNums + secondNums;
-    case "−":
-      return firstNums - secondNums;
-    case "×":
-    case "*":
-      return firstNums * secondNums;
-    case "÷":
-    case "/":
-      return firstNums / secondNums;
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(op, num1, num2) {
+  switch (op) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+    case '−':
+      return subtract(num1, num2);
+    case '*':
+    case '×':
+      return multiply(num1, num2);
+    case '/':
+    case '÷':
+      return divide(num1, num2);
     default:
-      return 0;
+      return 'Error in operate function';
   }
 }
 
-function updateScreen() {
-  var screenContent = "";
-  
-  screenContent += firstNums || "";
-  screenContent += operator ? " " + operator + " " : "";
-  screenContent += secondNums || "";
-  screenContent += currentNums.length !== 0 ? currentNums.join("") : "";
-  
-  screen.textContent = screenContent;
+function handleNumberButtonClick(clickedValue) {
+  if (operator === '') {
+    if (clickedValue === '.' && firstNum.includes('.')) {
+      return; // Ignore additional decimal points in firstNum
+    }
+    firstNum += clickedValue;
+    screen.textContent = firstNum;
+  } else {
+    if (clickedValue === '.' && secondNum.includes('.')) {
+      return; // Ignore additional decimal points in secondNum
+    }
+    secondNum += clickedValue;
+    screen.textContent = firstNum + ' ' + operator + ' ' + secondNum;
+  }
 }
 
-
-Array.from(numberButtons).forEach(function(button) {
-  button.addEventListener("click", function() {
-    var num = this.textContent;
-    if (!isNaN(parseFloat(num))) {
-      currentNums.push(parseFloat(num));
-      updateScreen();
-    } else if (num === "." && !decimalPressed) {
-      currentNums.push(".");
-      decimalPressed = true;
-      updateScreen();
-    }
-  });
-});
-
-
-document.getElementById("button-point").addEventListener("click", function() {
-  if (!decimalPressed) {
-    currentNums.push(".");
-    decimalPressed = true;
-    updateScreen();
-  }
-});
-
-Array.from(operatorButtons).forEach(function(button) {
-  button.addEventListener("click", function() {
-    if (currentNums.length > 0) {
-      operator = this.textContent.trim();
-      firstNums = convertToNumber();
-      currentNums = [];
-      decimalPressed = false;
-      updateScreen();
-    }
-  });
-});
-
-
-clearButton.addEventListener("click", function() {
-  firstNums = 0;
-  secondNums = 0;
-  currentNums = [];
-  operator = "";
-  decimalPressed = false;
-  updateScreen();
-});
-
-backspaceButton.addEventListener("click", function() {
-  currentNums.pop();
-  updateScreen();
-});
-
-equalsButton.addEventListener("click", function() {
-  secondNums = convertToNumber();
-  var result = calculateResult();
-  firstNums = 0;
-  secondNums = 0;
-  currentNums = [];
-  operator = "";
-  decimalPressed = false;
-  screen.textContent = result;
-});
-
-document.addEventListener("keydown", function(event) {
-  var key = event.key;
-  switch (key) {
-    case "Enter":
-      if (currentNums.length > 0) {
-        secondNums = convertToNumber();
-        var result = calculateResult();
-        firstNums = 0;
-        secondNums = 0;
-        currentNums = [];
-        operator = "";
-        decimalPressed = false;
+function handleOperatorButtonClick(clickedValue) {
+  if (operator === '') {
+    operator = clickedValue;
+    screen.textContent = firstNum + ' ' + operator;
+  } else {
+    if (secondNum !== '') {
+      const result = operate(operator.trim(), parseFloat(firstNum), parseFloat(secondNum));
+      if (result === 'Error') {
+        screen.textContent = 'Error';
+      } else {
         screen.textContent = result;
       }
-      break;
-    case "Backspace":
-      currentNums.pop();
-      updateScreen();
-      break;
-    case "Delete":
-      firstNums = 0;
-      secondNums = 0;
-      currentNums = [];
-      operator = "";
-      decimalPressed = false;
-      updateScreen();
-      break;
-    case "+":
-    case "-":
-    case "*":
-    case "/":
-      operator = key;
-      firstNums = convertToNumber();
-      currentNums = [];
-      decimalPressed = false;
-      updateScreen();
-      break;
-    default:
-      if (!isNaN(parseFloat(key))) {
-        currentNums.push(parseFloat(key));
-        updateScreen();
-      } else if (key === ".") {
-        if (!decimalPressed) {
-          currentNums.push(".");
-          decimalPressed = true;
-          updateScreen();
-        }
-      }
-      break;
+      firstNum = result.toString();
+      secondNum = '';
+    }
+    operator = clickedValue;
+    screen.textContent = firstNum + ' ' + operator;
   }
+}
+
+function handleEqualsButtonClick() {
+  if (firstNum !== '' && secondNum !== '') {
+    const result = operate(operator.trim(), parseFloat(firstNum), parseFloat(secondNum));
+    if (result === 'Error') {
+      screen.textContent = 'Error';
+    } else {
+      screen.textContent = result;
+    }
+    firstNum = result.toString();
+    secondNum = '';
+    operator = '';
+    hasDecimal = false; // Resetting the flag
+  }
+}
+
+function handleClearButtonClick() {
+  firstNum = '';
+  secondNum = '';
+  operator = '';
+  screen.textContent = '';
+}
+
+function handleBackspaceButtonClick() {
+  if (secondNum !== '') {
+    secondNum = secondNum.slice(0, -1);
+    screen.textContent = firstNum + ' ' + operator + ' ' + secondNum;
+  } else if (operator !== '') {
+    operator = '';
+    screen.textContent = firstNum;
+  } else {
+    firstNum = firstNum.slice(0, -1);
+    screen.textContent = firstNum;
+  }
+}
+
+function handleDotButtonClick() {
+  if (operator === '') {
+    if (!firstNum.includes('.')) {
+      firstNum += '.';
+      screen.textContent = firstNum;
+      hasDecimal = true;
+    }
+  } else {
+    if (!secondNum.includes('.')) {
+      secondNum += '.';
+      screen.textContent = firstNum + ' ' + operator + ' ' + secondNum;
+      hasDecimal = true;
+    }
+  }
+}
+
+function handleKeyboardInput(event) {
+  const { key } = event;
+
+  if (key >= '0' && key <= '9') {
+    handleNumberButtonClick(key);
+  } else if (key === '.') {
+    handleDotButtonClick();
+  } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+    handleOperatorButtonClick(key);
+  } else if (key === 'Enter') {
+    handleEqualsButtonClick();
+  } else if (key === 'Backspace') {
+    handleBackspaceButtonClick();
+  } else if (key === 'Delete') {
+    handleClearButtonClick();
+  }
+}
+
+[...numberButtons].forEach(button => {
+  button.addEventListener("click", function () {
+    const clickedValue = this.textContent;
+    handleNumberButtonClick(clickedValue);
+  });
 });
 
+[...operatorButtons].forEach(button => {
+  button.addEventListener("click", function () {
+    const clickedValue = this.textContent;
+    handleOperatorButtonClick(clickedValue);
+  });
+});
 
-updateScreen();
+equalsButton.addEventListener("click", handleEqualsButtonClick);
+clearButton.addEventListener("click", handleClearButtonClick);
+backspaceButton.addEventListener("click", handleBackspaceButtonClick);
+dot.addEventListener("click", handleDotButtonClick);
+
+document.addEventListener("keydown", handleKeyboardInput);
